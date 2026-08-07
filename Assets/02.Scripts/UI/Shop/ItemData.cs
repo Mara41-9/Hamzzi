1523c2213743286c019f6bb00c07bb74fe22f593
이나라 <dlskfk72@gmail.com>
2026-08-07 14:32:11 +0900

    [feat] 아이템 및 상점 데이터 추가
    - 추후 DB 연동 예정

---
+++ Assets/02.Scripts/UI/Shop/ItemData.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class ItemData : GameDataBase
+{
+    public string Name;
+    public string Description;
+    public string Category;
+    public string IconPath;
+    public string PrefabPath;
+}
