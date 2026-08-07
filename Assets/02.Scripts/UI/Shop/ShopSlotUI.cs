7b5d322e38b83317bea477ebc93f2caf4a382195
이나라 <dlskfk72@gmail.com>
2026-08-07 17:17:07 +0900

    [feat] 상점 UI 아이템 연동 및 선택 시 정보 표시 구현

---
+++ Assets/02.Scripts/UI/Shop/ShopSlotUI.cs
@@ -0,0 +1,79 @@
+﻿using Cysharp.Threading.Tasks;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopSlotUI : MonoBehaviour
+{
+    [SerializeField] private Image Image_Icon;
+    [SerializeField] private Image Image_Frame;
+    [SerializeField] private UIButton Button_Slot;
+
+    public long ItemSlotUniqueId { get; private set; }
+
+    public ItemData ItemData { get; private set; }
+
+    public Sprite IconSprite { get; private set; }
+
+    public event Action<long> OnClickItemSlot;
+
+    private void OnEnable()
+    {
+        Button_Slot.BindOnClickButtonEvent(OnClick_ItemSlot);
+    }
+
+    private void OnDisable()
+    {
+        Button_Slot.UnBindOnClickButtonEvent(OnClick_ItemSlot);
+    }
+
+    private void OnClick_ItemSlot()
+    {
+        OnClickItemSlot?.Invoke(ItemSlotUniqueId);
+        Debug.Log($"{ItemSlotUniqueId} 눌러졌다   아이템명: {ItemData.Name}");
+    }
+
+    public void SetIcon(string itemDataId)
+    {
+        ItemData = GameDataManager.Instance.GetData<ItemData>(itemDataId);
+        if(ItemData == null)
+        {
+            Debug.LogWarning("아이템 데이터를 찾을 수 없습니다.");
+            return;
+        }
+
+        InitImage().Forget();
+    }
+
+    public async UniTask InitImage()
+    {
+        string iconPath = ItemData.IconPath;
+        if (string.IsNullOrEmpty(iconPath) == true)
+        {
+            Debug.LogWarning("아이템 경로를 찾을 수 없습니다.");
+            return;
+        }
+
+        var loadedSprite = await ResourceManager.Instance.LoadAsset<Sprite>(iconPath);
+        if(loadedSprite == null)
+        {
+            Debug.LogWarning("아이템 경로에 따른 Sprite를 찾을 수 없습니다.");
+            return;
+        }
+
+        IconSprite = loadedSprite;
+
+        Image_Icon.sprite = IconSprite;
+    }
+
+    public void InitSlot(long slotUniqueId, string itemDataId)
+    {
+        ItemSlotUniqueId = slotUniqueId;
+        SetIcon(itemDataId);
+    }
+
+    public void BindSlotSelectEvent(Action<long> onClickItemSlot)
+    {
+        OnClickItemSlot += onClickItemSlot;
+    }
+}
---
+++ Assets/02.Scripts/UI/Shop/ShopUI.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopUI : MonoBehaviour
+{
+    [SerializeField] private GameObject Prefab_ItemSlot;
+    [SerializeField] private Transform Transform_SlotRoot;
+    [SerializeField] private Image Image_Icon;
+    [SerializeField] private TMP_Text Text_ItemName;
+    [SerializeField] private TMP_Text Text_ItemDescription;
+    [SerializeField] private TMP_Text Text_ItemPrice;
+
+    private long _slotUniqueId;
+    private ItemData _itemData;
+    private ShopSlotUI _shopSlotUI;
+
+    private Dictionary<long, ShopSlotUI> _itemSlotList = new Dictionary<long, ShopSlotUI>();
+    private List<ItemData> _itemDataList = new List<ItemData>();
+
+    private void Start()
+    {
+        TestLoadItemData();
+        SetShopItemSlotOnEnable();
+    }
+
+    private void OnEnable()
+    {
+        
+    }
+
+    private void TestLoadItemData()
+    {
+        GameDataManager.Instance.LoadData<ItemData>();
+
+        _itemDataList = GameDataManager.Instance.GetAllData<ItemData>();
+        if (_itemDataList == null)
+        {
+            Debug.LogWarning("아이템 데이터가 존재하지 않습니다.");
+            return;
+        }
+
+        foreach (var item in _itemDataList)
+        {
+            Debug.Log($"{item.Name}");
+        }
+    }
+
+    private void SetShopItemSlotOnEnable()
+    {
+        if(_itemDataList == null || _itemDataList.Count == 0)
+        {
+            Debug.LogWarning("보유한 아이템이 없습니다");
+            return;
+        }
+
+        foreach(var item in _itemDataList)
+        {
+            CreateItemSlot(_slotUniqueId, item.Id);
+            _slotUniqueId++;
+        }
+    }
+
+    private void CreateItemSlot(long slotUniqueId, string itemDataId)
+    {
+        var gObj = Instantiate(Prefab_ItemSlot, Transform_SlotRoot);
+        if(gObj == null)
+        {
+            return;
+        }
+
+        _shopSlotUI = gObj.GetComponent<ShopSlotUI>();
+        if(_shopSlotUI == null)
+        {
+            return;
+        }
+
+        _shopSlotUI.InitSlot(slotUniqueId, itemDataId);
+
+        _itemSlotList.Add(slotUniqueId, _shopSlotUI);
+        _shopSlotUI.BindSlotSelectEvent(OnChildSlotSelected);
+    }
+
+    private void OnChildSlotSelected(long slotUniqueId)
+    {
+        if(_itemSlotList.TryGetValue(slotUniqueId, out _shopSlotUI) == false)
+        {
+            return;
+        }
+
+        _itemData = _shopSlotUI.ItemData;
+
+        Image_Icon.sprite = _shopSlotUI.IconSprite;
+        Text_ItemName.text = _itemData.Name;
+        Text_ItemDescription.text = _itemData.Description;
+    }
+    
+}
