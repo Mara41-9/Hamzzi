a33eb8c327aa4f584428601d669217921e4202a3
유민형 <minhyeong61022@gmail.com>
2026-08-12 16:19:22 +0900

    [feat] 1회 뽑기 및 10회 뽑기 구현

--- Assets/02.Scripts/Features/Gacha/GachaView.cs
+++ Assets/02.Scripts/Features/Gacha/GachaView.cs
@@ -3,43 +3,56 @@ using UnityEngine.UI;
 
 public class GachaView : ViewBase
 {
-    [SerializeField] private Button GachaButton;
+    [Header("UI Base")]
+    [SerializeField] private Button ExitButton;
+
+    [SerializeField] private Button DrawOneButton;
+    [SerializeField] private Button DrawTenButton;
+
+    private CollectionViewModel _collectionViewModel;
+
+    private void Start()
+    {
+        _collectionViewModel = NetworkManager_YMH.Instance.CollectionService.GetCollectionViewModel();
+    }
 
     private void OnEnable()
     {
-        GachaButton.onClick.AddListener(DrawHamster);
+        ExitButton.onClick.AddListener(CloseCollectionUI);
+
+        DrawOneButton.onClick.AddListener(DrawHamster);
+        DrawTenButton.onClick.AddListener(DrawTenHamster);
     }
 
     private void OnDisable()
     {
-        GachaButton.onClick.RemoveListener(DrawHamster);
+        ExitButton.onClick.RemoveListener(CloseCollectionUI);
+
+        DrawOneButton.onClick.RemoveListener(DrawHamster);
+        DrawTenButton.onClick.RemoveListener(DrawTenHamster);
+    }
+
+    private void CloseCollectionUI()
+    {
+        UIManager.Instance.CloseUI(UIRootType.PopupUI, UIType.GachaUI);
     }
 
     private void DrawHamster()
     {
-        int SSCount = 0;
-        int SCount = 0;
-        int ACount = 0;
-        for(int i = 0; i < 1000; i++)
+        string hamsterId = NetworkManager_YMH.Instance.GachaService.DrawGacha();
+        Debug.Log(hamsterId);
+
+        if (_collectionViewModel.CollectedHamsterIdList.Contains(hamsterId) == false)
         {
-            string hamsterId = NetworkManager_YMH.Instance.GachaService.DrawGacha();
-            HamsterData hamsterData = GameDataManager.Instance.GetData<HamsterData>(hamsterId);
-            Debug.Log($"햄스터 가챠 결과 : {hamsterData.Name}({hamsterData.HamsterTier})");
-
-            switch (hamsterData.HamsterTier)
-            {
-                case HamsterTier.SS:
-                    SSCount++;
-                    break;
-                case HamsterTier.S:
-                    SCount++;
-                    break;
-                case HamsterTier.A:
-                    ACount++;
-                    break;
-            }
+            _collectionViewModel.CollectedHamsterIdList.Add(hamsterId);
         }
+    }
 
-        Debug.Log($"총 횟수 : 1000,  SS : {SSCount},  S : {SCount},  A : {ACount}");
+    private void DrawTenHamster()
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            DrawHamster();
+        }
     }
 }
