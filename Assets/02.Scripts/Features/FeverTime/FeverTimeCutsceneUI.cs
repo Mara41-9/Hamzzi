dab443b249cdf4b2c18375021bfdbb5199c51ccd
최진영 <jychoi194@gmail.com>
2026-08-12 14:46:02 +0900

    [feat] 피버타임 컷신 UI 뼈대 구현

---
+++ Assets/02.Scripts/Features/FeverTime/FeverTimeCutsceneUI.cs
@@ -0,0 +1,21 @@
+﻿// 피버타임 컷신(영상 재생 자리)과 연타 버튼을 담당하는 UI
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FeverTimeCutsceneUI : UIBase
+{
+    [SerializeField] private RawImage RawImage_Cutscene;
+    [SerializeField] private TMP_Text Text_TapGuide;
+    [SerializeField] private UIButton Button_Tap;
+
+    private void OnEnable()
+    {
+        Button_Tap.BindOnClickButtonEvent(OnClickTap);
+    }
+
+    private void OnClickTap()
+    {
+        // TODO: HAM-67 연타 카운트 로직 연결되면 여기서 FeverTimeManager에 카운트 등록 호출
+    }
+}
