2882f38b28ce9e6bee6f71dcb0851529fd2783db
ClairvoyantHG <wjdgory@gmail.com>
2026-08-13 17:51:58 +0900

    [feat] 친구 목록 로직 추가

---
+++ Assets/02.Scripts/Account/FriendListView.cs
@@ -0,0 +1,108 @@
+﻿using System.ComponentModel;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FriendListView : UIBase
+{
+    [SerializeField] private Transform Transform_Content; 
+    [SerializeField] private GameObject Prefab_FriendSlot; 
+    [SerializeField] private UIButton Button_Close;
+
+    private FriendListViewModel _vm;
+
+    private List<GameObject> _spawnedSlots = new List<GameObject>();
+
+
+    // 임시 테스트용 초기화
+    private void Start()
+    {
+        FriendListService testService = new FriendListService();
+        FriendListViewModel testVm = new FriendListViewModel();
+
+        testVm.SetService(testService);
+
+        BindViewModel(testVm);
+    }
+
+    private void OnEnable()
+    {
+        Button_Close.BindOnClickButtonEvent(OnClickClose);
+
+        if (_vm != null)
+        {
+            _vm.RequestLoadFriendList();
+        }
+    }
+
+    private void OnDisable()
+    {
+        ClearSpawnedSlots();
+    }
+
+    private void OnDestroy()
+    {
+        if (_vm != null)
+        {
+            _vm.PropertyChanged -= OnPropChanged_View;
+            _vm.OnCompleteLoadFriendList -= OnCompleteLoadFriendList_View;
+        }
+    }
+
+    public void BindViewModel(FriendListViewModel vm)
+    {
+        _vm = vm;
+
+        _vm.PropertyChanged += OnPropChanged_View;
+        _vm.OnCompleteLoadFriendList += OnCompleteLoadFriendList_View;
+    }
+
+    private void OnPropChanged_View(object sender, PropertyChangedEventArgs e)
+    {
+
+    }
+
+    private void OnClickClose()
+    {
+        UIManager.Instance.CloseUI(UIRootType.PopupUI, UIType.FriendListUI);
+    }
+
+    private void OnCompleteLoadFriendList_View()
+    {
+        ClearSpawnedSlots();
+
+        if (_vm != null)
+        {
+            int count = _vm.FriendList.Count;
+
+            for (int i = 0; i < count; i++)
+            {
+                FriendInfoData friendData = _vm.FriendList[i];
+
+                GameObject slotObj = Instantiate(Prefab_FriendSlot, Transform_Content);
+                FriendSlotUI slotView = slotObj.GetComponent<FriendSlotUI>();
+
+                if (slotView != null)
+                {
+                    slotView.SetFriendData(friendData);
+                }
+
+                _spawnedSlots.Add(slotObj);
+            }
+        }
+    }
+
+    private void ClearSpawnedSlots()
+    {
+        int count = _spawnedSlots.Count;
+
+        for (int i = 0; i < count; i++)
+        {
+            if (_spawnedSlots[i] != null)
+            {
+                Destroy(_spawnedSlots[i]);
+            }
+        }
+
+        _spawnedSlots.Clear();
+    }
+}
---
+++ Assets/02.Scripts/Account/FriendSlotUI.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class FriendSlotUI : UIBase
+{
+    [SerializeField] private TextMeshProUGUI TextMesh_FriendName;
+    [SerializeField] private TextMeshProUGUI TextMesh_FriendId;
+    [SerializeField] private UIButton Button_Visit;
+
+    private string _friendId = "";
+
+    public void SetFriendData(FriendInfoData data)
+    {
+        if (data != null)
+        {
+            TextMesh_FriendName.text = data.FriendName;
+            TextMesh_FriendId.text = data.FriendId;
+            _friendId = data.FriendId;
+        }
+    }
+
+    private void OnEnable()
+    {
+        Button_Visit.BindOnClickButtonEvent(OnClickVisit);
+    }
+
+    private void OnDisable()
+    {
+
+    }
+
+    private void OnClickVisit()
+    {
+        Debug.Log($"방문하기 기능 대기 상태입니다. 대상 ID: {_friendId}");
+    }
+}
