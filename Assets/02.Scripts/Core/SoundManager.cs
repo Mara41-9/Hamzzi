d841e3ccb0bbb793cd967c6c56b59aadc3d16b3f
이주현 <juhyun040100@gmail.com>
2026-08-18 13:20:56 +0900

    [feat] SoundManager 추가

---
+++ Assets/02.Scripts/Core/SoundManager.cs
@@ -0,0 +1,54 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public class SoundManager : SingletonBase<SoundManager>
+{
+    [SerializeField] private AudioSource AudioSource_BGM;
+    [SerializeField] private AudioSource AudioSource_SFX;
+
+    private async UniTaskVoid LoadAndPlayAudioClip(AudioSource audioSource, string path, bool isLoop = false)
+    {
+        AudioClip clip = await ResourceManager.Instance.LoadAsset<AudioClip>(path);
+
+        if (isLoop)
+        {
+            audioSource.clip = clip;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+        else
+        {
+            audioSource.PlayOneShot(clip);
+        }
+    }
+
+    private string GetPathSFX(string soundID)
+    {
+        return $"Audio/SFX/{soundID}";
+    }
+
+    private string GetPathBGM(string soundID)
+    {
+        return $"Audio/BGM/{soundID}";
+    }
+
+    public void PlaySFX(string soundID)
+    {
+        LoadAndPlayAudioClip(AudioSource_SFX, GetPathSFX(soundID)).Forget();
+    }
+
+    public void PlayBGM(string soundID)
+    {
+        LoadAndPlayAudioClip(AudioSource_BGM, GetPathBGM(soundID), true).Forget();
+    }
+
+    public void StopSFX()
+    {
+        AudioSource_SFX.Stop();
+    }
+
+    public void StopBGM()
+    {
+        AudioSource_BGM.Stop();
+    }
+}
