d99c1addc1c453da6b9d475777b174f2dc7e982e
이주현 <juhyun040100@gmail.com>
2026-08-18 16:50:10 +0900

    [fix] UI 테스트 코드 제거

--- Assets/02.Scripts/Core/UIManager.cs
+++ Assets/02.Scripts/Core/UIManager.cs
@@ -16,7 +16,6 @@ public class UIManager : SingletonBase<UIManager>
     private void Start()
     {
         OpenUI(UIRootType.MainUI, UIType.TitleUI);
-        OpenUI(UIRootType.MainUI, UIType.TestUI);
     }
 
     public UIBase OpenUI(UIRootType uiRootType, UIType uiType, bool isInitialHide = false)
