4497f6fb44e86437f117c131f21308993a3459c7
최진영 <jychoi194@gmail.com>
2026-08-21 14:41:17 +0900

    [feat] NavMesh 런타임 베이크 매니저 - NavigationManager 추가

---
+++ Assets/02.Scripts/Core/NavigationManager.cs
@@ -0,0 +1,25 @@
+﻿// 지하굴 통로의 NavMesh 생성, 재베이크를 관리하는 매니저
+using UnityEngine;
+using Unity.AI.Navigation;
+
+public class NavigationManager : SingletonBase<NavigationManager>
+{
+    private const KeyCode RebakeKey = KeyCode.N;
+
+    [SerializeField] private NavMeshSurface _navMeshSurface;
+
+    private void Update()
+    {
+#if UNITY_EDITOR
+        if (Input.GetKeyDown(RebakeKey))
+        {
+            RebuildNavMesh();
+        }
+#endif
+    }
+
+    public void RebuildNavMesh()
+    {
+        _navMeshSurface.BuildNavMesh();
+    }
+}
