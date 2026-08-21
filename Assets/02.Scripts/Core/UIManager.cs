9c9bf8843a7e7ce50439f80fb6a8ca8d18255608
이나라 <dlskfk72@gmail.com>
2026-08-21 17:04:50 +0900

    [feat] 로딩 UI 및 로딩 기능 구현

--- Assets/02.Scripts/Core/UIManager.cs
+++ Assets/02.Scripts/Core/UIManager.cs
@@ -12,9 +12,10 @@ public class UIManager : SingletonBase<UIManager>
     private Dictionary<UIType, UIBase> _createdUIList = new Dictionary<UIType, UIBase>();
     private HashSet<UIType> _opendUIList = new HashSet<UIType>();
 
-    // [나라] TODO : 게임 시작하자마자 Title UI 열리도록
+    // [나라] TODO : 게임 시작하자마자 LoadingUI 열리도록
     private void Start()
     {
+        OpenUI(UIRootType.VeryFrontUI, UIType.LoadingUI);
         OpenUI(UIRootType.MainUI, UIType.TitleUI);
     }
 
