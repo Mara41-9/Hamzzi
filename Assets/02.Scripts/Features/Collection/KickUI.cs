c8e257be4e26e528d22f3f63e53fe9ec9ad22831
유민형 <minhyeong61022@gmail.com>
2026-08-23 19:59:28 +0900

    [feat] 햄스터 방출 시 재확인 UI 구현

---
+++ Assets/02.Scripts/Features/Collection/KickUI.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+public class KickUI : MonoBehaviour
+{
+    [SerializeField] private UIButton KickButton;
+    [SerializeField] private UIButton ExitButton;
+
+    private CollectionViewModel _collectionViewModel;
+
+    private void Start()
+    {
+        _collectionViewModel = ServiceManager.Instance.CollectionService.GetCollectionViewModel();
+    }
+
+    private void OnEnable()
+    {
+        KickButton.BindOnClickButtonEvent(OnClickKickButton);
+        ExitButton.BindOnClickButtonEvent(OnClickExitButton);
+    }
+
+    private void OnDisable()
+    {
+        KickButton.UnBindOnClickButtonEvent(OnClickKickButton);
+        ExitButton.UnBindOnClickButtonEvent(OnClickExitButton);
+    }
+
+    private void OnClickKickButton()
+    {
+        string currentHamsterId = _collectionViewModel.CurrentSelectHamsterId;
+        string currentfaceId = _collectionViewModel.CurrentSelectedHamsterFaceId;
+
+        _collectionViewModel.RemoveCollectedHamsterList(currentHamsterId, currentfaceId);
+
+        gameObject.SetActive(false);
+    }
+
+    private void OnClickExitButton()
+    {
+        gameObject.SetActive(false);
+    }
+}
