3f4ae03225f6f7f02013a18226ff22e7f30460ce
유민형 <minhyeong61022@gmail.com>
2026-08-27 16:08:06 +0900

    [feat] 외형 보여주기 용 햄스터를 따로 Service로 분리

---
+++ Assets/02.Scripts/Features/ModelViewr/HamsterModelService.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class HamsterModelService
+{
+    private HamsterModelViewModel _modelViewrViewModel;
+
+    public HamsterModelViewModel GetHamsterModelViewModel()
+    {
+        if (_modelViewrViewModel == null)
+        {
+            var modelViewrViewModel = new HamsterModelViewModel();
+            SetHamsterModelViewModel();
+            _modelViewrViewModel = modelViewrViewModel;
+        }
+
+        return _modelViewrViewModel;
+    }
+
+    private void SetHamsterModelViewModel()
+    {
+        GameObjectManager.Instance.CreateObject(string.Empty, "Hamster3DModel", Vector3.one * 1000);
+    }
+}
